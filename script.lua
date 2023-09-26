180e4cd7b9b58fe3777acbb19c0d424baadcc9cf
DookDekDEE <75850456+DookDekDEE@users.noreply.github.com>
2023-09-26 23:11:28 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -3,15 +3,15 @@ if placeId == 2753915549 or placeId == 4442272183 or placeId == 7449423635 then
 	--Blox Fruit
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/DookDekDEE/All-Script/main/bf.lua"))()
 elseif placeId == 4520749081 or placeId == 6381829480 or placeId == 5931540094 then
-    --King Legacy
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/DookDekDEE/All-Script/main/kl.lua"))()
+	--King Legacy
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/DookDekDEE/All-Script/main/kl.lua"))()
 elseif placeId == 6777872443 then
-    --Pixel Piece
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/DookDekDEE/All-Script/main/pp.lua"))()
+    	--Pixel Piece
+    	loadstring(game:HttpGet("https://raw.githubusercontent.com/DookDekDEE/All-Script/main/pp.lua"))()
 elseif placeId == 12192552089 then
 	--Fruit Warriors
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/DookDekDEE/All-Script/main/fw.lua"))()
 elseif placeId == 6918802270 then
 	--HAZE PIECE
-loadstring(game:HttpGet("https://raw.githubusercontent.com/DookDekDEE/All-Script/main/hp.lua"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/DookDekDEE/All-Script/main/hp.lua"))()
 end
