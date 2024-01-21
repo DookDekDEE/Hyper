fd110cd85924a5e2d3b56bf56db031cb23ec77eb
DookDekDEE <75850456+DookDekDEE@users.noreply.github.com>
2024-01-21 14:16:34 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -2,7 +2,7 @@ local placeId = game.PlaceId
 if placeId == 2753915549 or placeId == 4442272183 or placeId == 7449423635 then
 	--Blox Fruit
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/DookDekDEE/All-Script/main/bf.lua"))()
-elseif placeId == 4520749081 or placeId == 6381829480 or placeId == 5931540094 then
+elseif placeId == 4520749081 or placeId == 6381829480 or placeId == 5931540094 or placeId == 15759515082 then
 	--King Legacy
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/DookDekDEE/All-Script/main/kl.lua"))()
 elseif placeId == 6777872443 then
