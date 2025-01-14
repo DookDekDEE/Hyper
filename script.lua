4d8beae2eb93d5451a76e0e62a245b1d25b3fdf2
DookDekDEE <75850456+DookDekDEE@users.noreply.github.com>
2025-01-14 09:59:44 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -5,8 +5,9 @@ if placeId == 2753915549 or placeId == 4442272183 or placeId == 7449423635 then
 	loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/9b875e079c7a4e29acc2ff103454567f.lua"))()
 elseif placeId == 4520749081 or placeId == 6381829480 or placeId == 5931540094 or placeId == 15759515082 then
 	--King Legacy
-	loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/af1f18a5c81eb310b9d48415d795891d.lua"))()
+	--loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/af1f18a5c81eb310b9d48415d795891d.lua"))()
 	--loadstring(game:HttpGet("https://raw.githubusercontent.com/DookDekDEE/All-Script/refs/heads/main/kingl.lua"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/DookDekDEE/All-Script/refs/heads/main/oldkl.lua"))()
 elseif placeId == 6777872443 then
     	--Pixel Piece
     	loadstring(game:HttpGet("https://raw.githubusercontent.com/DookDekDEE/All-Script/main/pp.lua"))()
@@ -27,6 +28,6 @@ elseif placeId == 18337464872 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/DookDekDEE/All-Script/main/wp.lua"))()
 elseif placeId == 89438510123061 then
 	--Anime Shadow
-	--loadstring(game:HttpGet("https://raw.githubusercontent.com/DookDekDEE/All-Script/main/shadow.lua"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/DookDekDEE/All-Script/main/shadow.lua"))()
 	--loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/8ca4ec399bb51f74879cbdef584641ce.lua"))()
 end
